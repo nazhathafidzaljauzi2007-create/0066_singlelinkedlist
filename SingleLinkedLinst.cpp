d7a9d582b33f685716da55f5951828abe43f15de
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-23 19:36:47 +0700

    Node

--- SingleLinkedLinst.cpp
+++ SingleLinkedLinst.cpp
@@ -36,5 +36,56 @@ public:
         START = nodeBaru;
         return; 
     }
-    
-}
+
+    Node *previous = START;
+    Node *current = START;
+
+    while (current != NULL && nim > current->noMhs)
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodebaru->next = current;
+    previous->next = nodeBaru;
+
+    }
+
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        
+            delete current;
+            return true;
+    }
