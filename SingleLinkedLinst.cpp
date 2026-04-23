5a8e6b709990bc1f3b640bee5af360ff772c6264
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-23 19:43:57 +0700

    void traverse

--- SingleLinkedLinst.cpp
+++ SingleLinkedLinst.cpp
@@ -93,5 +93,20 @@ public:
 
     void traverse()
     {
-        
+        if (listEmpty())
+        {
+            cout << "\nList Kosongan\n";
+        }
+        else
+        {
+            cout << "\nData di dalam list adalah:\n";
+            Node *currentNode = START;
+
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode-next;
+            }
+            cout << endl;
+        }
     }
